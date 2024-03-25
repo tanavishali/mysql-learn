8b19c6d3a8bc27acc2159d174fcd1021e4811117
tanavishali <tanawishalrai5271@gmail.com>
2024-03-25 15:19:43 +0500

    add

---
+++ creat table.sql
@@ -0,0 +1,7 @@
+create table student(
+id integer,
+name text,
+phoneNumber integer,
+city text
+);
+-- in this way we can creat table 
---
+++ select into.sql
@@ -0,0 +1,5 @@
+insert into student(id,name,phoneNumber,city)
+values(01,"Ali",000001,"okara"),
+(02,"Rahman",000013,"Lahore");
+-- select * from student;
+-- select name from student;
